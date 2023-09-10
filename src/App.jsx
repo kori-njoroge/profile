eecaed1f279f8ac0d3eb88f8c7a3571e6ac7d191
kori njoroge <korinjoroge63@gmail.com>
2023-09-10 20:37:30 +0300

    labels added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import profile from "./assets/kori.jpeg";
+import git from "./assets/git.png";
 
 export default function App() {
   const [currentDate, setCurrentDate] = useState(null);
+  const track = "Frontend";
   const days = [
     "Sunday",
     "Tuesday",
@@ -19,6 +21,7 @@ export default function App() {
       const utcDate = now.toISOString();
       const utcDay = now.getUTCDay();
       const timeMilliseconds = now.getTime();
+      // const timeMilliseconds = now.getMilliseconds
 
       setCurrentDate({ utcDate, utcDay, timeMilliseconds });
     }, 1000);
@@ -43,15 +46,18 @@ export default function App() {
         <div>
           {currentDate && (
             <div>
-              <p data-testid="currentDayOfTheWeek">
+              <p className="collapse">Today:&nbsp;</p>
+              <p className="collapse bold" data-testid="currentDayOfTheWeek">
                 {days[currentDate.utcDay]}
-              </p>
-              <p data-testid="currentUTCTime">{currentDate.timeMilliseconds}</p>
+              </p> <br />
+              <p className="collapse">Time:&nbsp;&nbsp;</p>
+              <p className="collapse bold" data-testid="currentUTCTime">{currentDate.timeMilliseconds}</p>
             </div>
           )}
         </div>
         <div>
-          <p data-testid="myTrack">Frontend</p>
+          <p className="collapse">Track:&nbsp;&nbsp;</p>
+          <p className="collapse bold" data-testid="myTrack">{track}</p>
         </div>
         <div class="gitprofile">
           <a
@@ -59,7 +65,7 @@ export default function App() {
             href="https://github.com/kori-njoroge/profile"
             target="_blank"
           >
-            Github URL
+            <img className="git-logo" src={git} alt="githubURL"></img>
           </a>
         </div>
       </div>
